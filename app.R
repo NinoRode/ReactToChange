a8b1695a2955d569de83c1f88555f5aec75ea9d0
NinoRode <nino.rode@gmail.com>
2020-03-09 22:02:56 +0100

    Pravilni razpored stolpcev

--- app.R
+++ app.R
@@ -34,11 +34,12 @@ server <- shinyServer(function(input, output, session) {
     # setwd(directory)
     # mes_df <- read_a_file("10-16feb20LucijaMetelko.xlsx")
     zac_tedna <-  Sys.Date() - as.numeric(format(Sys.Date(), "%u")) + 1
-    mes_df <- data.frame("datum" = seq(zac_tedna, by = "day", length.out = 7),
-                                   "zacetek" = rep(8, 7),
-                                   "konec" = rep(14, 7),
-                                   "odsotnost" = rep("", 7),
-                                   "delovni čas" = rep(8, 7))
+    mes_df <- data.frame("datum" = seq(zac_tedna, by = "day", length.out = 7))
+    mes_df$dan <- weekdays( mes_df$datum)
+    mes_df$zacetek <- c(rep(8, 5), NA, NA)
+    mes_df$konec <- c(rep(14, 5), NA, NA)
+    mes_df$odsotnost <- rep("", 7)
+    mes_df$delovni_cas <-  mes_df$konec -  mes_df$zacetek
 
     previous <- reactive({mes_df})
 
