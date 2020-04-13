f47e747cdaed84fca6d5d1e9b3f9773cf3f292dd
NinoRode <nino.rode@gmail.com>
2020-04-13 08:47:15 +0200

    Minimal MWE

--- min_wrk_examp.R
+++ min_wrk_examp.R
@@ -1,33 +1,24 @@
 library(shiny)
 library(rhandsontable)
 
-tdn <- seq(Sys.Date()+1, by = "day", length.out = 7)
-table1 <- data.frame(day = weekdays(tdn+1),
-                datum = as.character(tdn, "%e. %b. %Y"),
-                beginning = as.numeric(rep(NA, 7)),
-                ending = as.numeric(rep(NA, 7)),
-                hours = as.numeric(rep(NA, 7)))
+data <- list (
+  table1 = data.frame( beginning = as.numeric(rep(8, 4)),
+                ending = as.numeric(rep(15, 4))),
 
-tdn <- seq(Sys.Date()+1, by = "day", length.out = 7)
-table2 <-data.frame(day = weekdays(tdn+1),
-                datum = as.character(tdn, "%e. %b. %Y"),
-                beginning = as.numeric(rep(NA, 7)),
-                ending = as.numeric(rep(NA, 7)),
-                hours = as.numeric(rep(NA, 7)))
+table2 = data.frame( beginning = as.numeric(rep(9, 4)),
+                ending = as.numeric(rep(17, 4)))
+)
 
+data$table1$hours <- data$table1$ending - data$table1$beginning
+data$table2$hours <- data$table2$ending - data$table2$beginning
 ############################# UI #############################
 
 ui = shinyUI(fluidPage(
   selectInput("tab", "Chose table: ", choices = list("table1", "table2")),
-  dateInput("week", "chose date:",
-            value = Sys.Date() - as.numeric(format(Sys.Date(), "%u")) + 8,
-            format = "DD, dd. M. yyyy",
-            language = "sl",
-            weekstart = 1),
   fluidRow(wellPanel(
     column(6,
            rHandsontableOutput("hot"),
-           actionButton(inputId="enter",label="Shrani urnik")
+           actionButton(inputId="enter",label="Save")
     ),
 
     column(6,
@@ -42,37 +33,20 @@ server=function(input,output, session){
 
   tab_change <- reactiveVal(FALSE)
 
-  zac_tedna <-  reactive(input$week - as.numeric(format(input$week, "%u")) + 1) # postavi na začetek tedna (+1, ne +8)
-  observe(
-    updateDateInput(session, "week", value = zac_tedna())
-  )
 
-   file_name <- reactive(paste(input$tab, format(input$week, "-%d-%b"), ".csv", sep = ""))
-
-   observe( if(!file.exists(file_name())) {
-     write.csv2(df, file_name(), row.names = FALSE)
-     df
-   })
-
-   rw <- reactiveFileReader(1000, session, file_name, read.csv2)
+   # rw <- reactivePoll(1000, session, file_name, read.csv2)
    react_week <- reactive({
-     df <- as.data.frame(rw())
-     df[3:5] <-  sapply(3:5, function (i) as.numeric(df[, i]))
-     df
+     df <- data[[input$tab]]
      })
-
-  output$week <- renderTable(
-    react_week())
 
   output$title <- renderText(input$tab)
 
   output$tabela <- renderTable(react_week())
 
-  observeEvent(list(input$tab, input$week),
-               tab_change(TRUE))
+  observeEvent(input$tab, tab_change(TRUE))
 
 
-  # Calculation of columns from https://stackoverflow.com/questions/44074184/reactive-calculate-columns-in-rhandsontable-in-shiny-rstudio
+  # Calculation of columns
   for_week <- reactive({
 
     datacopy <- NULL
@@ -88,28 +62,31 @@ server=function(input,output, session){
     #If there is change in data
     if(!is.null(input$hot$changes$changes)){
 
-      # row.no <- as.numeric(unlist(input$hot$changes$changes)[1])
       col.no <- as.numeric(unlist(input$hot$changes$changes)[2])
       new.val <- unlist(input$hot$changes$changes)[4]
 
       #If the changed value is prihod or odhod
-      if(col.no == 2 || col.no == 3){
-        datacopy[, 5] <- as.numeric(datacopy[, 4]) - as.numeric(datacopy[2, 3])
+      if(col.no == 0 || col.no == 1){
+        datacopy[, 3] <- as.numeric(datacopy[, 2]) - as.numeric(datacopy[, 1])
       }
 
     }
 
     tab_change(FALSE)
-
+print(str(input$hot))
     datacopy
 
   })
+  rhot <- eventReactive(list(input$tab,input$hot), rhandsontable(for_week()))
 
-  output$hot <- renderRHandsontable({
-    rhandsontable(for_week())
+  output$hot <- renderRHandsontable(
+    rhot()
+  )
+
+  observeEvent(input$enter, {
+    data[[input$tab]] <- hot_to_r(input$hot)
+    output$tabela <- renderTable( data[[input$tab]])
   })
-
-  observeEvent(input$enter, write.csv2(hot_to_r(input$hot), file_name(), row.names = FALSE))
 
 }
 
