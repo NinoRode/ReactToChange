5fdf57a9bbc20287c787bd89a84a21e4f9d2c6fb
NinoRode <nino.rode@gmail.com>
2021-05-02 11:49:29 +0200

    Limit multitude to accomodate available data. More test data (400 points in 7 dimensions).

--- divide_hull.R
+++ divide_hull.R
@@ -1,13 +1,27 @@
 
-pnt7 <- read.csv2("data/tek_onako.csv")
 multitude <- 8
 
-centr <- colMeans(pnt7) #compute centroid
-cnt_pnt7 <-pnt7 - centr # Move to origin
+pntz <- read.csv2("data/tek_onako.csv")
+dimz <- ncol(pntz)
+np <- nrow(pntz)
 
-min_pnt7 <- mapply(min, cnt_pnt7)
-di <- max(c(- min_pnt7,  mapply(max, cnt_pnt7)))
+centr <- colMeans(pntz) #compute centroid
+cnt_pntz <-pntz - centr # Move to origin
 
-prt <- di/multitude
-cube <- t(sapply(0:(2 * multitude), function(x) min_pnt7 + x * prt))
+min_pntz <- mapply(min, cnt_pntz)
+di <- max(c(- min_pntz,  mapply(max, cnt_pntz)))
 
+if(np < multitude * multitude * dimz * 5) {
+  sprintf("Warning: Number of points (%d) is too small for the proposed multitude (%d)", np, multitude)
+  multitude <- 2^floor(log2(sqrt(np / dimz / 5)))
+  sprintf("multitude changed to %d", multitude)
+}
+
+if(multitude < 2) {
+  #------------------------  STOP IF TOO FIEW POINTS  ------------------------#
+  stop("Number of points is too small.\n use simple quickhull")
+  
+} else {
+  prt <- di/multitude
+  cube <- t(sapply(0:(2 * multitude), function(x) min_pntz + x * prt))
+}
