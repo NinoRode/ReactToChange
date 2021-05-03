43a6b9ff490263d16204b9a3c67af597e9af034f
NinoRode <nino.rode@gmail.com>
2021-05-03 10:09:07 +0200

    Testing script. Finding two smallest values

---
+++ testbed.R
@@ -0,0 +1,8 @@
+d <- c(12, 13, 10, 9, 4, 25, 8, 26, 51, 7, 11, 32)
+
+two_min <- vector(typeof(d), 2)
+which_min <- vector("integer", 2)
+two_min[1] <- min(d)
+which_min[1] <- which(d == two_min[1])
+two_min[2] <- min(d[-which_min[1]])
+which_min[2] <- which(d == two_min[2])
