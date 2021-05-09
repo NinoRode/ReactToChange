6a55a6cc7ea0a0cf29f82b0a07f8e7f978ac9372
NinoRode <nino.rode@gmail.com>
2021-05-09 18:03:27 +0200

    When more points have same extreme coordinate, use only the most distant point.

--- tower_build_hull.R
+++ tower_build_hull.R
@@ -1,13 +1,22 @@
 library(data.tree)
 
-pntz <- read.csv2("data/tek_onako.csv")
+p <- read.csv2("data/tek_onako.csv")
+pntz <- p[, 1:4]
 dimz <- ncol(pntz)
 np <- nrow(pntz)
 
 cnt_pntz <- as.data.frame(scale(pntz))
 cnt_pntz$nrm <- vapply(1:np, function(i) sqrt(sum(cnt_pntz[i, ]^2)), double(1)) #fast and dirty norms
 maxs <- vapply(cnt_pntz, max, double(1)) #safer and faster than sapply
-mins <- vapply(cnt_pntz, max, double(1))
+mins <- vapply(cnt_pntz, min, double(1))
 
-faces <- rbind(cnt_pntz[which(sapply(1:nrow(cnt_pntz), function(i) any(maxs %in% cnt_pntz[i, ]))), ],
-               cnt_pntz[which(sapply(1:nrow(cnt_pntz), function(i) any(mins %in% cnt_pntz[i, ]))), ])
+hll_max <- cnt_pntz[which(vapply(1:np, function(i) {any(maxs[1:dimz] %in% cnt_pntz[i, ])}, logical(1))), ]
+hll_min <- cnt_pntz[which(vapply(1:np, function(i) {any(mins[1:dimz] %in% cnt_pntz[i, ])}, logical(1))), ]
+
+max_max <- vapply(1:dimz, function(i) {max(hll_max[which(hll_max[, i] == maxs[i]), dimz+1])}, 
+                  double(1))
+hll_max <- hll_max[vapply(hll_max[, dimz+1], function (i) {i %in% max_max }, logical(1)), ]
+
+max_min <- vapply(1:dimz, function(i) {max(hll_min[which(hll_min[, i] == mins[i]), dimz+1])}, 
+                  double(1))
+hll_min <- hll_min[vapply(hll_min[, dimz+1], function (i) {i %in% max_min }, logical(1)), ]
