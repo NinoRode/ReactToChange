2a8a7c66bc00cba0911231d9250321851d9aef7f
NinoRode <nino.rode@gmail.com>
2021-05-13 11:17:20 +0200

    new comments norm

--- tower_build_hull.R
+++ tower_build_hull.R
@@ -6,10 +6,13 @@ dimz <- ncol(pntz)
 np <- nrow(pntz)
 
 cnt_pntz <- as.data.frame(scale(pntz))
-cnt_pntz$nrm <- vapply(1:np, function(i) sqrt(sum(cnt_pntz[i, ]^2)), double(1)) #fast and dirty norms
-maxs <- vapply(cnt_pntz, max, double(1)) #safer and faster than sapply
-mins <- vapply(cnt_pntz, min, double(1))
+cnt_pntz$nrm <- vapply(1:np, function(i) sqrt(sum(cnt_pntz[i, ]^2)), double(1)) # fast and dirty norms
+# cnt_pntz$nrm <- vapply(1:np, function(i) norm(cnt_pntz[i, ]), double(1)) # official norms
+# vapply is safer and faster than sapply
+maxs <- vapply(cnt_pntz, max, double(1)) # find max for each dimension
+mins <- vapply(cnt_pntz, min, double(1)) # find min for each dimension
 
+# find the points with max/min value
 hll_max <- cnt_pntz[which(vapply(1:np, function(i) {any(maxs[1:dimz] %in% cnt_pntz[i, ])}, logical(1))), ]
 hll_min <- cnt_pntz[which(vapply(1:np, function(i) {any(mins[1:dimz] %in% cnt_pntz[i, ])}, logical(1))), ]
 
