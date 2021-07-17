b2e9104110f1b6a63c693116823ce94b2009e6db
NinoRode <nino.rode@gmail.com>
2021-07-17 17:38:24 +0200

    excelR

---
+++ excelR_primer.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(excelR)
+
+shinyApp(
+  ui = fluidPage(excelOutput("table")),
+  server = function(input, output, session) {
+    output$table <-
+      renderExcel(excelTable(data = head(iris)))
+  }
+)
+
+shinyApp(
+  ui = fluidPage(excelOutput("table")),
+  server = function(input, output, session) {
+    output$table <-
+      renderExcel(excelTable(data = head(iris)))
+    observeEvent(input$table,{
+      table_data <- excel_to_R(input$table)
+      if(!is.null(table_data)){
+        print(table_data)
+      }
+
+    })
+  }
+)
