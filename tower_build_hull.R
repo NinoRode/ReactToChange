ef3d23a7ab05c7ff2d9d2393a5f4885c73f9836f
NinoRode <nino.rode@gmail.com>
2021-08-13 18:15:39 +0200

    i'm back

--- tower_build_hull.R
+++ tower_build_hull.R
@@ -136,6 +136,68 @@ find_sky_line <- function(pntz, to_origin = TRUE) {
   return(skyline)
 }
 
+#' find_hull0 <- function(pntz, to_origin = TRUE) {
+#'   #' Finds the upper convex hull of the data
+#'   
+#'   dimz <- ncol(pntz)
+#'   np <- nrow(pntz)
+#'   vec_norm <- function(i) sqrt(sum(i^2))
+#'   
+#'   if(is.vector(pntz)) {
+#'     pntz <- as.matrix(t(pntz))
+#'   } else {
+#'     pntz <- as.matrix(pntz)
+#'   }
+#'   
+#'   colMin <- apply(pntz, 2, min)
+#'   
+#'   if(to_origin) {
+#'     pntz <- sweep(pntz, 2, colMin)
+#'   }
+#'   skyline <- data.frame(matrix(ncol = dimz, nrow = 0))
+#'   colnames(skyline) <- paste0("x", 1:dimz)
+#'   
+#'   repeat {
+#'     maxs <- apply(pntz, 2, max) # find max for each dimension
+#'     skln <- pntz[which(vapply(1:np, function(i) {any(maxs[1:dimz] %in% pntz[i, ])}, logical(1))), ]
+#'     
+#'     skln_nrm <- apply(skln, 1, vec_norm) # fast and dirty norms
+#'     max_max <- vapply(1:dimz, function(i) {
+#'       max(skln_nrm[which(skln[, i] == max(skln[, i]))])
+#'     }, double(1))
+#'     skln <- skln[vapply(skln_nrm, function (i) {i %in% max_max }, logical(1)), ]
+#'     
+#'     skyline <- rbind(skyline, skln)
+#'     pntz_over <- pntz[!is_it_same_side(pntz, skln), ]
+#'     np <- nrow(pntz_over)
+#'     if (is.null(np)) {
+#'       skyline <- rbind(skyline, pntz_over)
+#'       skyline <- sweep(skyline, 2, colMin, FUN = "+")
+#'       
+#'       return(skyline)
+#'       
+#'     }
+#'     if (np <= dimz) {
+#'       pnt_nrm <- apply(pntz_over, 1, vec_norm)
+#'       top_pnt <- pntz_over[which(pnt_nrm == max(pnt_nrm)), ]
+#'       skyline <- rbind(skyline, top_pnt)
+#'       for (i in 1:dimz) {
+#'         tmp <- !is_it_same_side(pntz_over, rbind(skln[-i, ], top_pnt))
+#'         if (sum(tmp) > 0) skyline <- rbind(skyline, tmp)
+#'       }
+#'       skyline <- sweep(skyline, 2, colMin, FUN = "+")
+#'       
+#'       return(skyline)
+#'       
+#'     } else {
+#'       pntz <- pntz_over
+#'     }
+#'   }
+#'   
+#'   skyline <- sweep(skyline, 2, colMin, FUN = "+")
+#'   return(skyline)
+#' }
+
 find_hull <- function(pntz, to_origin = TRUE) {
   #' Finds the upper convex hull of the data
   
@@ -212,7 +274,12 @@ np <- nrow(pntz)
 
 find_sky_line(test2)
 find_hull(test2)
-find_sky_line(pntz)
+
+pntz <- scale(pntz, scale = FALSE)
+
+pntz <- pntz[apply(pntz, 1, all())]
+
+find_sky_line((pntz))
 find_hull(pntz)
 
 cnt_pntz <- as.data.frame(scale(pntz))
