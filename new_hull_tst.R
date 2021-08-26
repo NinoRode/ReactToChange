f11a4e4f71489fdbe1a53eb6cd8982e1d12eb8ed
NinoRode <nino.rode@gmail.com>
2021-08-26 14:17:14 +0200

    New level building. If multiple max, add points without max from previous level to create a wedge

---
+++ new_hull_tst.R
@@ -0,0 +1,330 @@
+library(geometry)
+library(matrixcalc)
+
+############################################################################
+############################################################################
+############# >> , drop = FALSE] PREPREČI SPREMEMBO V VEKTOR  << ###########
+############################################################################
+############################################################################
+
+hull_algo <- function(p_hull_pntz) {
+  convhulln(p_hull_pntz, options = "Tv", output.options = NULL,
+            return.non.triangulated.facets = FALSE)
+}
+
+drop_rows_one <- function(pntz, rows = NULL) {
+  
+  dim_num <- length(dim(pntz)) + 1
+  if (dim_num == 1)
+    max_row <- length(pntz)
+  else
+    max_row <- dim(pntz)[1]
+  
+  stopifnot("Too many dimensions: only 3-dim supported" = dim_num < 5)
+  stopifnot("Rows do not exist" = max(rows) <= max_row, min(rows) > 0)
+  
+  switch(dim_num,
+         pntz <- pntz[-rows],
+         pntz <- pntz[-rows],
+         pntz <- pntz[-rows, ], 
+         pntz <- pntz[-rows, , ] 
+  )
+}
+
+put_zeros <- function(pntz, drop, mask) { ######################################### debug
+  
+  if (!is.matrix(mask)) {mask <- t(as.matrix(mask))}
+  sapply (1:length(drop), function (i) 
+    {zero_candidates <- which(mask[i, ] == 1) 
+    to_zero <- sample(zero_candidates, length(zero_candidates) - 1)
+    # print(c(drop[i], to_zero))
+    pntz[drop[i], to_zero] <<- 0
+    # print(pntz)
+    }
+  )
+  pntz
+}
+
+decide <- function(levl, give = "one") {
+  #' Decide which vector (row) has the highest norm.
+  #' It can give the longest vector, or all but the shortest vector.
+
+  nrm <- sqrt(rowSums(levl^2)) # sqrt(drop(i %*% i)) but faster
+  switch(give,
+         all = {delt <- which.min(nrm)
+         levl <- levl[-delt, , drop = FALSE]},
+         one = {delt <- which.max(nrm)
+         levl <- levl[delt, , drop = FALSE]}
+  )
+}
+
+change_position <- function(tmp_pntz, in_position) {
+  rws <- nrow(tmp_pntz)
+  ip <- matrix(rep(in_position, rws), nrow = rws, byrow = TRUE)
+  tmp_pntz <- tmp_pntz * ip
+  tmp_pntz
+}
+
+get_level <- function(pntz, prev_level) {
+ 
+  dimz <- ncol(pntz)
+  np <- nrow(pntz)
+  levl <- matrix(numeric(), nrow = 0, ncol = dimz)
+  mask <- NULL
+  
+  while (nrow(levl) < dimz && np > 0) {
+    
+    # print(nrow(levl)) ########################################################## debug
+    #..........................................................................#
+    # Najdi točke z največjo vednostjo ("zgornje") za vsako dimenzijo
+    #..........................................................................#
+    if(is.null(mask) || sum(mask) == 0) mask <- rep(1, dimz)
+    
+    # cmpr <- 1 : dimz * mask
+    # 
+    # cmpr <- cmpr[cmpr != 0]
+    # nc <- length(cmpr)
+    
+    maxs <- apply(pntz, 2, max) # find max for each dimension
+    num_max <- rowSums(t(vapply(1:np, function(i) {(maxs %in% pntz[i, ])}, numeric(dimz))))
+    # num_max <- rowSums(t(vapply(1:np, function(i) {(maxs[cmpr] %in% pntz[i, cmpr])}, numeric(nc))))
+    # max_pos <- which(has_max == 1, arr.ind = TRUE)
+    # 
+    # mask <- has_max[max_pos[which(duplicated(max_pos[ , "row"])), "row"], ] # Pozor, če jih je več postane matrika #####
+    # if (is.matrix(mask)) mask <- ifelse(colSums(mask) > 0, 1, 0)
+    
+    #..........................................................................#
+    # Pripravi večkratne zgornje in jih očisti iz podatkov 
+    #..........................................................................#
+    # if (any(num_max > 1)){
+    #   to_zero <- which(num_max > 1)
+    #   for_levl <- (pntz[to_zero, ]) ###################################### upam, da dbl očisti to
+    #   
+    #   pntz <- put_zeros(pntz, to_zero, has_max[to_zero, ])
+    #   # num_max <- drop_rows_one(num_max, drop)
+    # } else {
+    # }
+    for_levl <- pntz[which(num_max > 0), , drop = FALSE]
+    
+    # if (!is.matrix(for_levl)) for_levl <- t(as.matrix(for_levl))
+    #..........................................................................#
+    # Počisti dvojnike
+    #..........................................................................#
+    # dbl <- unlist(apply(for_levl, 2, function(x) {which(x == x[which(duplicated(x))])})) 
+    # if(length(dbl) > 0){
+    #   for_levl <- decide(for_levl[dbl, ])
+    # }
+    
+    from_prev <- (nrow(for_levl) + 1):dimz
+    
+    levl <- rbind(for_levl, prev_level[from_prev, ])
+    
+    pntz <- drop_rows_one(pntz, which(pntz %in% levl))
+    # levl <- levl[!duplicated(levl), ]
+    # while (nrow(levl) > dimz) {
+    #   levl <- decide(levl, give = "all")
+    # }
+    # 
+    # # if (!is.matrix(levl)) levl <- t(as.matrix(levl))
+    # if (nrow(levl) > dimz) {
+    #   maxs <- apply(levl, 2, max) # find max for each dimension
+    #   levl <- levl[vapply(1:nrow(levl), function(i) {any(maxs %in% pntz[i, ])}, numeric(1)), ]
+    # 
+    # }
+    # 
+    # dbl <- unlist(apply(levl, 2, function(x) {which(x == x[which(duplicated(x))])})) 
+    # if(length(dbl) > 0){
+    #   levl <- rbind(levl, decide(levl[dbl, , drop = FALSE]))
+    #   levl <- levl[-dbl, , drop = FALSE]
+    #   levl
+    # }
+    # 
+    #..........................................................................#
+    # Popravi vektor v matriko
+    #..........................................................................#
+    # if (!is.matrix(pntz)) pntz <- t(as.matrix(pntz)) 
+    
+    #..........................................................................#
+    np <- nrow(pntz)
+    
+  }
+  
+  
+  levl # Procedura, ki kliče, mora preveriti, če je levl kompleten
+}
+
+find_points_over <- function(pntz, one_level) {
+  collin_test <- qr(one_level)
+  while (collin_test$rank < ncol(one_level)) {
+  
+  }
+  
+  pntz[is_it_same_side(pntz, one_level, other = TRUE), ]
+}
+
+is_it_same_side <- function(pntz, facet, eye = NULL, other = FALSE) {
+  #' Determines if the points are on the same/other side of 
+  #' the (hyperplane of the) facet 
+  #' as the eye (or on the hyperplane).
+  #' @param pntz: points to be tested
+  #' @param eye: observers eye, origin of the view
+  #' @param facet: data frame or matrix of points determining a hyperplane of the facet
+  #' @param other: boolean determines if the same or other side is true
+  
+  if (!is.null(eye)) {
+    pntz <- pntz - eye
+    facet <- facet - eye
+  }
+  
+  if (!is.matrix(pntz)) {
+    if(is.vector(pntz)) {
+      pntz <- as.matrix(t(pntz))
+    } else {
+      pntz <- as.matrix(pntz)}
+  }
+  
+  if (!is.matrix(facet)) {
+    facet <- as.matrix(facet)
+  }
+  
+  f <- solve (facet, rep(1, ncol(facet)))
+  
+  pos <- vapply(1:nrow(pntz), function(x) 
+  {round(t(pntz[x, ] - facet[1, ]) %*% f, 12)}, double(1))
+  
+  if(other) {
+    return(ifelse(pos <= 0, FALSE, TRUE ))
+  } else {
+    return(ifelse(pos <= 0, TRUE, FALSE ))
+  }
+}
+
+build_tower <- function(pntz, one_level) {
+  
+  # find_sky_line <- function(pntz, to_origin = FALSE) {
+  #' Finds the sky line of the data
+  
+  if(!is.matrix(pntz)) {
+    if(is.vector(pntz)) {
+      pntz <- as.matrix(t(pntz))
+    } else {
+      pntz <- as.matrix(pntz)
+    }
+  }
+  
+  dimz <- ncol(pntz)
+  np <- nrow(pntz)
+  
+  # if(to_origin) {
+  #   colMin <- apply(pntz, 2, min)
+  #   pntz <- sweep(pntz, 2, colMin)
+  # }
+  
+  p_hull_pntz <- one_level
+  colnames(p_hull_pntz) <- paste0("x", 1:dimz)
+  
+  while (np >= dimz) {
+    
+    pntz_over <- find_points_over(pntz, one_level)
+    
+    # np <- nrow(pntz_over)
+    # if (is.null(np)) np <- 1
+    
+    if (length(pntz_over) > 0)  { ############### stvar gre do konca ###############
+      
+      # p_hull_pntz <- rbind(p_hull_pntz, pntz_over) ######## stvar gre do konca ###
+      
+    # } else {
+      
+      pntz <- pntz_over
+      one_level <- get_level(pntz, one_level)
+      p_hull_pntz <- rbind(p_hull_pntz, one_level) # put in p_hull_pntz
+    }
+  }
+  
+  return(p_hull_pntz)
+}
+
+build_p_hull <- function(pntz) {
+  
+  cnter <- colMeans(pntz)
+  tmp_pntz <- scale(pntz, center = cnter, scale = FALSE)
+  plot(tmp_pntz)
+  
+  first_max  <- get_level(tmp_pntz) # find points with maximum on each dimension
+  mc <- max.col(first_max)
+  first_max <- first_max[mc, ]
+  # p_hull_pntz <- first_max
+  
+  tmp <- build_tower(tmp_pntz, first_max)
+  p_hull_pntz <- tmp 
+  points(tmp,pch = 19)
+  
+  tmp_pntz <- -tmp_pntz
+  first_min  <- get_level(tmp_pntz) # find points with minimum on each dimension
+  first_min <- first_min[max.col(first_min), ]
+  # p_hull_pntz <- rbind(p_hull_pntz, -first_min)
+  
+  tmp <- build_tower(tmp_pntz, first_min)
+  tmp <- -tmp
+  p_hull_pntz <- rbind(p_hull_pntz, tmp) 
+  points(tmp,pch = 19)
+  
+  tmp_pntz <- -tmp_pntz
+  first_min <- -first_min
+  
+  #vse kombinacije
+  for (x in 1:ncol(tmp_pntz)) {
+    one_level <- rbind(first_max[x, ], first_min[-x, ])
+    in_position <- sign(colMeans(one_level))
+    tmp_pntz <- change_position(tmp_pntz, in_position) 
+    one_level <- change_position(one_level, in_position) 
+    tmp <-  build_tower(tmp_pntz, one_level)
+    tmp <- change_position(tmp, in_position)  
+    tmp_pntz <- change_position(tmp_pntz, in_position) 
+    p_hull_pntz <- rbind(p_hull_pntz, tmp)
+    points(tmp,pch = 19, col = x)
+    
+    one_level <- rbind(first_max[-x, ], first_min[x, ])
+    in_position <- sign(colMeans(one_level))
+    tmp_pntz <- change_position(tmp_pntz, in_position) 
+    one_level <- change_position(one_level, in_position) 
+    tmp <-  build_tower(tmp_pntz, one_level)
+    tmp <- change_position(tmp, in_position) 
+    tmp_pntz <- change_position(tmp_pntz, in_position) 
+    p_hull_pntz <- rbind(p_hull_pntz, tmp)
+    points(tmp,pch = 19, col = x)
+  }
+  
+  p_hull_pntz <- unique(p_hull_pntz)
+  p_hull_pntz <- p_hull_pntz + matrix(rep(cnter, nrow(p_hull_pntz)), nrow = nrow(p_hull_pntz), byrow = TRUE)
+}
+
+build_hull <- function(build_p_hull) {
+  
+  p_hull_pntz <- build_p_hull(pntz)
+  tst <- hull_algo(p_hull_pntz)
+  # x <- matrix(numeric(), nrow = 0, ncol = ncol(p_hull_pntz))
+  # cntr <- 1:nrow(tst)
+  # for (i in cntr) {
+  #   x <- rbind(x, p_hull_pntz[tst[i, 1], ], p_hull_pntz[tst[i, 2], ])
+  # }
+  list(tst, p_hull_pntz)
+}
+
+p <- as.data.frame(matrix(rnorm(10000), ncol = 100))
+pntz <- p[, 1:5]
+
+# pntz <- read.csv2("/home/nino/git/div_hull/data/test_data_prblm.csv", row.names = 1)
+
+p_hull_pntz <- build_p_hull(pntz)
+p_hull_pntz
+sts <- (hull_algo(p_hull_pntz))
+sts
+sort(unique(as.vector(sts)))
+
+tst <- (hull_algo(pntz))
+tst
+ 
+sort(unique(as.vector(tst)))
+sort(unique(as.vector(sts)))
